c80057e2434e074a3cd713f21d992ba863b5866b
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2016-08-27 13:56:23 +0100

    deletes unused class

--- src/main/java/uk/co/mruoc/fake/github/CannotBuilderUriException.java
+++
@@ -1,9 +0,0 @@
-package uk.co.mruoc.fake.github;
-
-public class CannotBuilderUriException extends RuntimeException {
-
-    public CannotBuilderUriException(Throwable cause) {
-        super(cause);
-    }
-
-}
